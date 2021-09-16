5160920be71d1b5abd0fc9480e7573b8e90e7d9a
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-09-16 11:06:27 -0700

    updated welcome page, some minor css fixes

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,41 @@ const Hiker = hikeSchemas.Hiker;
 
 //Welcome Page
 router.get('/', (req, res) => {
-  res.render('welcome', { title: 'Welcome' })
+  const email = 'andrew.arensman@gmail.com'
+
+  Hiker.findOne({ email: email })
+  .then(result => {
+    //duplicating the logs to avoid the error with _id
+    var hikesArray = result.log;
+    var newHikesArray = []
+
+    hikesArray.forEach(hikeObj => {
+      var hike = (({ hike_name, hike_date, mileage, time, elevation_gain, min_elevation, max_elevation, average_pace, average_bpm, max_bpm, city, location, notes }) => ({ hike_name, hike_date, mileage, time, elevation_gain, min_elevation, max_elevation, average_pace, average_bpm, max_bpm, city, location, notes }))(hikeObj);
+
+      var idStr = hikeObj._id.toString()
+      hike.id = idStr;
+
+      newHikesArray.push(hike)
+    })
+
+    // Adding Date and Fixing Format of hike_date
+    newHikesArray.forEach(i => {
+      var newDate = new Date(i.hike_date.replace(/-/g, '\/'))
+      i['real_date'] = newDate;
+      i['hike_date'] = newDate.toLocaleDateString('en-US');  //Converts to MM/DD/YYYY
+    })
+
+    // Sorting Hikes by date
+    newHikesArray.sort((a, b) => a.real_date - b.real_date)
+
+    var userId = result._id.toString()
+
+    res.render('welcome', { data: newHikesArray, username: 'Andy', user_id: userId, title: 'Welcome', isExample: 'Yes', new_user: 'No' })
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(404).render('404');
+  })
 })
 
 //dashboard
