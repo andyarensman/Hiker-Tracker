93f478441959915b6066655a07cf5e14257984fb
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-10-13 11:36:01 -0700

    new email address for password reset and footer

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -132,9 +132,9 @@ const forgot_handle = (req, res, next) => {
       });
       var mailOptions = {
         to: user.email,
-        from: 'andrew.arensman@gmail.com',
+        from: 'hiking.data.contact@gmail.com',
         subject: 'Hike Data Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your hiking account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
           'http://' + req.headers.host + '/users/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
